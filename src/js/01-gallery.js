cb1a76916bd839a95fa106d1c8456ed266bb1e88
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-05-08 00:42:43 +0200

    add 1 and 2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+        <div>
+        <a class="gallery__item" href="${original}">
+           <img class="gallery__image" src="${preview}" 
+           title="${description}" alt="${description}" />
+        </a>
+        </div>`
+        })
+        .join('');
+}
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a');
+
+galleryContainer.addEventListener('click', lightbox.on('show.simplelightbox', function () {
+	lightbox.defaultOptions.captionDelay = 250;
+}));
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
 
+import _ from "lodash";
+import Player from '@vimeo/player';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', _.throttle(updateTime, 1000));
+
+function updateTime ({seconds}) {
+   localStorage.setItem("videoplayer-current-time", seconds);        
+}
+
+const time = Number(localStorage.getItem("videoplayer-current-time"));      
+
+
+player.setCurrentTime(time).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
