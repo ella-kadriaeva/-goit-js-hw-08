1ee9ad4a56052d29a9e93f3d405b794bf1636101
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-05-08 22:46:26 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 import _ from "lodash";
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = {
+    email: '',
+    message: '',
+};
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onSubmitForm);
+form.addEventListener('input', _.throttle(onFildInput, 500));
+
+
+populateForm ()
+
+function onFildInput (e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm (e) {
+    e.preventDefault();
+    if(!JSON.parse(localStorage.getItem(STORAGE_KEY))) {
+        return
+    }
+        console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+        e.currentTarget.reset();
+        localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateForm () {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+   
+    if(savedData) {
+        const arreyData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        form.email.value = arreyData.email;
+        form.message.value = arreyData.message;   
+    }
+}
